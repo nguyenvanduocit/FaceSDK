8b1f0c0767b00e94704ac0c657dd7cecfd1abc48
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-10-12 09:56:08 +0700

    Support Group node

---
+++ src/FaceSDK/Node/Group.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nguyenvanduocit
+ * Date: 10/12/2015
+ * Time: 9:54 AM
+ */
+
+namespace FaceSDK\Node;
+
+
+class Group extends Node
+{
+    /**
+     * @var array Maps object key names to Graph object types.
+     */
+    protected static $objectMap = [
+        'person' => '\FaceSDK\Node\Person'
+    ];
+
+    /**
+     * @return \FaceSDK\Node\Person
+     */
+    public function getPersons(){
+        return $this->getField('person');
+    }
+}
